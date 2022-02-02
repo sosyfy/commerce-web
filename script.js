bafb1bc9c40558f860f03012616c386a75189607
Sospeter Karanja <98221088+sosyfy@users.noreply.github.com>
2022-02-02 15:51:26 +0300

    fix responsive issues on navbar

---
+++ script.js
@@ -0,0 +1,12 @@
+const bar = document.getElementById('bar')
+const nav = document.getElementById('navbar')
+const close = document.getElementById('close')
+
+if(bar){
+    bar.addEventListener('click', ()=>{
+        nav.classList.add('active');
+    })
+}
+close.addEventListener('click', function(){
+    nav.classList.remove('active')
+})
