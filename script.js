864e6da6302b7f9e6a06702b29a18a36e48972ff
Sospeter Karanja <98221088+sosyfy@users.noreply.github.com>
2022-02-03 19:04:40 +0300

    added shop page,footer ,product description and annimations

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ if(bar){
 close.addEventListener('click', function(){
     nav.classList.remove('active')
 })
+
+const bigImage =document.getElementById('main-img')
+const smallImage = document.getElementsByClassName('small-img')
+
+smallImage[0].onclick = ()=>{
+    bigImage.src = smallImage[0].src;
+}
+
+smallImage[1].onclick = ()=>{
+    bigImage.src = smallImage[1].src;
+}
+
+ smallImage[2].onclick = ()=>{
+    bigImage.src = smallImage[2].src;
+}
+
+ smallImage[3].onclick = ()=>{
+    bigImage.src = smallImage[3].src; 
+}
+
+// for (i=0 ; i < smallImage.length; i++){
+//     smallImage[i].onclick = ()=>{
+//         bigImage.src = smallImage[i].src;
+//     }
+// }
+ 
